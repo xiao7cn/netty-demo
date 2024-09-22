4f2cbc40321ab8aba5fd59ea11faac8ae6dbfd59
miller.siuq <miller.siuq@finpoints.com>
2024-09-22 23:32:37 +0800

    init

--- src/main/java/org/example/TestPipeline.java
+++ src/main/java/org/example/TestPipeline.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
 
 @Slf4j
 public class TestPipeline {
@@ -21,25 +26,55 @@ public class TestPipeline {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 log.debug("1");
-                                super.channelRead(ctx, msg);
+                                ByteBuf buf = (ByteBuf) msg;
+                                String name = buf.toString(Charset.defaultCharset());
+                                super.channelRead(ctx, name);
                             }
                         });
                         pipeline.addLast("h2", new ChannelInboundHandlerAdapter() {
                             @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            public void channelRead(ChannelHandlerContext ctx, Object name) throws Exception {
                                 log.debug("2");
-                                super.channelRead(ctx, msg);
+                                Student student = new Student(name.toString());
+                                super.channelRead(ctx, student);
                             }
                         });
                         pipeline.addLast("h3", new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                log.debug("3");
-                                super.channelRead(ctx, msg);
+                                log.debug("3, 结果{}, class:{}", msg, msg.getClass());
+                                ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server...".getBytes()));
+                            }
+                        });
+                        pipeline.addLast("h4", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h5", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                        pipeline.addLast("h6", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("6");
+                                super.write(ctx, msg, promise);
                             }
                         });
                     }
                 })
                 .bind(8080);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Student {
+        private String name;
     }
 }
