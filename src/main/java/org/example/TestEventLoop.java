46b8b5418c7ed4465aa595acca23083b00b880ad
miller.siuq <miller.siuq@finpoints.com>
2024-09-22 16:22:56 +0800

    init

--- src/main/java/org/example/TestEventLoop.java
+++ src/main/java/org/example/TestEventLoop.java
@@ -1,30 +1,64 @@
 package org.example;
 
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class TestEventLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         EventLoopGroup group = new NioEventLoopGroup(2);
-        System.out.println(group.next());
-        System.out.println(group.next());
-        System.out.println(group.next());
-        System.out.println(group.next());
 
-        group.next().submit(() -> {
-            log.debug("11");
+        CountDownLatch countDownLatch = new CountDownLatch(8);
+        group.submit(() -> {
+            log.debug("aa {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        group.submit(() -> {
+            log.debug("bb {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        group.submit(() -> {
+            log.debug("cc {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        group.submit(() -> {
+            log.debug("dd {}", Thread.currentThread());
+            countDownLatch.countDown();
         });
 
-        group.next().submit(() -> {
-            log.debug("22");
+        EventLoop eventLoop1 = group.next();
+        eventLoop1.submit(() -> {
+            log.debug("11 {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        EventLoop eventLoop2 = group.next();
+        eventLoop2.submit(() -> {
+            log.debug("22 {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        EventLoop eventLoop3 = group.next();
+        eventLoop3.submit(() -> {
+            log.debug("33 {}", Thread.currentThread());
+            countDownLatch.countDown();
+        });
+        EventLoop eventLoop4 = group.next();
+        eventLoop4.submit(() -> {
+            log.debug("44 {}", Thread.currentThread());
+            countDownLatch.countDown();
         });
 
-        group.next().submit(() -> {
-            log.debug("33");
-        });
+        group.next().scheduleAtFixedRate(() -> {
+            log.debug("ok");
+        }, 0, 100, TimeUnit.MILLISECONDS);
 
+        countDownLatch.await();
         log.debug("main");
+
+        group.shutdownGracefully();
     }
 }
